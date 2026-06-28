67691f1eb15348e496c05e19fcf5d47da2c44c4b
DanDanDaniels <danielvictor1608@gmail.com>
2026-06-27 21:24:42 -0300

    Creating Mediator pattern Enenmies destruction

--- code/Const.py
+++ code/Const.py
@@ -21,6 +21,25 @@ ENTITY_SPEED = {
     'Enemy2': 1,
 
 }
+# H
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player1Shot': 1,
+    'Player2': 300,
+    'Player2Shot': 1,
+    'Enemy1': 50,
+    'Enemy1Shot': 1,
+    'Enemy2': 60,
+    'Enemy2Shot': 1,
+
+}
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -39,8 +58,7 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
-
-#S
+# S
 SPAWN_TIME = 4000
 
 # W
